856a759553c89deabb8b5ca03693aff7832f15cd
Alex <jzsplking@gmail.com>
2021-04-11 17:06:26 +0800

    ✨ add func for create NOde

--- src/dom.rs
+++ src/dom.rs
@@ -1,14 +1,16 @@
-use std::collections::HashMap;
+use std::collections::{HashMap,HashSet};
+
+pub type AttrMap = HashMap<String, String>
 
 struct Node {
     // data common to all nodes
-    children: Vec<None>,
+    children: Vec<Node>,
 
     // data specific to each node type:
     node_type: NodeType,
 }
 
-enum NodeType {
+pub enum NodeType {
     Text(String),
     Element(ElementData)
 }
@@ -18,4 +20,20 @@ struct ElementData {
     attributes: AttrMap
 }
 
-type AttrMap = HashMap<String, String>
+// construct functions
+fn text(content: String) -> Node {
+    Node {
+        children: Vec::new(), 
+        node_type: NodeType::Text(content)
+    }
+}
+
+fn elem(name: String, attrs: AttrMap, children: Vec<Node>) -> Node {
+    Node {
+        children: children,
+        node_type: NodeType::Element(ElementData {
+            tag_name: name,
+            attributes: attrs,
+        })
+    }
+}
