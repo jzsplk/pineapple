6eb1b76a02e021453260121427f33fea130de616
Alex <jzsplking@gmail.com>
2021-04-11 18:48:49 +0800

    ✨ add mod for dom

--- src/dom.rs
+++ src/dom.rs
@@ -1,34 +1,37 @@
-use std::collections::{HashMap,HashSet};
+use std::collections::HashMap;
 
-pub type AttrMap = HashMap<String, String>
+pub type AttrMap = HashMap<String, String>;
 
-struct Node {
+#[derive(Debug)]
+pub struct Node {
     // data common to all nodes
-    children: Vec<Node>,
+    pub children: Vec<Node>,
 
     // data specific to each node type:
-    node_type: NodeType,
+    pub node_type: NodeType,
 }
 
+#[derive(Debug)]
 pub enum NodeType {
     Text(String),
     Element(ElementData)
 }
 
-struct ElementData {
+#[derive(Debug)]
+pub struct ElementData {
     tag_name: String,
     attributes: AttrMap
 }
 
 // construct functions
-fn text(content: String) -> Node {
+pub fn text(content: String) -> Node {
     Node {
         children: Vec::new(), 
         node_type: NodeType::Text(content)
     }
 }
 
-fn elem(name: String, attrs: AttrMap, children: Vec<Node>) -> Node {
+pub fn elem(name: String, attrs: AttrMap, children: Vec<Node>) -> Node {
     Node {
         children: children,
         node_type: NodeType::Element(ElementData {
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
+pub mod dom;
+
 fn main() {
     let a: u32 = 12; 
+    let _test_dom = dom::text(String::from("test node"));
     println!("Hello, world! {}", a);
+    println!("text dom {:?}", _test_dom.node_type)
 }
