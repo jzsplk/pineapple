4c675fad7bead7cb921dfc1e0a7fc3f01c3d7a43
Alex <jzsplking@gmail.com>
2021-04-11 19:10:31 +0800

    ✨ try print node

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,13 @@ use std::collections::HashMap;
 pub mod dom;
 
 fn main() {
-    let a: u32 = 12; 
     let mut class_name = HashMap::new();
+
     class_name.insert(String::from("display"), String::from("block"));
+
     let _text_dom = dom::text(String::from("text node"));
-    let _ele_dom = dom::elem(String::from("div"),class_name, Vec::new());
-    println!("Hello, world! {}", a);
-    println!("text dom {:?}", _text_dom);
+    let children = vec![_text_dom];
+    let _ele_dom = dom::elem(String::from("div"),class_name, children);
+
     println!("ele dom {:?}", _ele_dom);
 }
